59fc35a6f694ee97498c7a005d085d5071be3000
kamehamehotdogs <omondr@pm.me>
2024-02-14 08:36:47 -0500

    .

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Step 1: Loading Steam Release Branch
 echo "Loading Steam Release Branch"
 bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
                     +login anonymous \
@@ -8,61 +9,64 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 
 if [ $? -eq 0 ]; then
 
-  LOCAL_CONFIG="~/PalWorldSettings.ini"
   DEST_PATH="${STEAMAPPDIR}/Pal/Saved/Config/LinuxServer"
   DEST_CONFIG="${DEST_PATH}/PalWorldSettings.ini"
+  DEFAULT_CONFIG="${STEAMAPPDIR}/DefaultPalWorldSettings.ini"
 
-  replace_config_value() {
-    local key="$1"
-    local value="$2"
-    local env_var="$3"
-
-    if [ -n "${!env_var}" ]; then
-      local new_value="${!env_var}"
-
-      sed -i "s|^\($key=\).*|\1$new_value|" "$LOCAL_CONFIG"
-    fi
-  }
-
-replace_config_value "Difficulty" "$DIFFICULTY" "DIFFICULTY"
-replace_config_value "DayTimeSpeedRate" "$DAY_TIME_SPEED_RATE" "DAY_TIME_SPEED_RATE"
-replace_config_value "NightTimeSpeedRate" "$NIGHT_TIME_SPEED_RATE" "NIGHT_TIME_SPEED_RATE"
-replace_config_value "ExpRate" "$EXP_RATE" "EXP_RATE"
-replace_config_value "PalCaptureRate" "$PAL_CAPTURE_RATE" "PAL_CAPTURE_RATE"
-replace_config_value "PalDamageRateAttack" "$PAL_DAMAGE_RATE_ATTACK" "PAL_DAMAGE_RATE_ATTACK"
-replace_config_value "PalDamageRateDefense" "$PAL_DAMAGE_RATE_DEFENSE" "PAL_DAMAGE_RATE_DEFENSE"
-replace_config_value "PlayerDamageRateAttack" "$PLAYER_DAMAGE_RATE_ATTACK" "PLAYER_DAMAGE_RATE_ATTACK"
-replace_config_value "PlayerDamageRateDefense" "$PLAYER_DAMAGE_RATE_DEFENSE" "PLAYER_DAMAGE_RATE_DEFENSE"
-replace_config_value "PlayerStomachDecreaseRate" "$PLAYER_STOMACH_DECREASE_RATE" "PLAYER_STOMACH_DECREASE_RATE"
-replace_config_value "PlayerStaminaDecreaseRate" "$PLAYER_STAMINA_DECREASE_RATE" "PLAYER_STAMINA_DECREASE_RATE"
-replace_config_value "PalStomachDecreaseRate" "$PAL_STOMACH_DECREASE_RATE" "PAL_STOMACH_DECREASE_RATE"
-replace_config_value "PalStaminaDecreaseRate" "$PAL_STAMINA_DECREASE_RATE" "PAL_STAMINA_DECREASE_RATE"
-replace_config_value "CollectionDropRate" "$COLLECTION_DROP_RATE" "COLLECTION_DROP_RATE"
-replace_config_value "DeathPenalty" "$DEATH_PENALTY" "DEATH_PENALTY"
-replace_config_value "bEnableInvaderEnemy" "$ENABLE_INVADER_ENEMY" "ENABLE_INVADER_ENEMY"
-replace_config_value "BaseCampWorkerMaxNum" "$BASE_CAMP_WORKER_MAX_NUM" "BASE_CAMP_WORKER_MAX_NUM"
-replace_config_value "PalEggDefaultHatchingTime" "$PAL_EGG_DEFAULT_HATCHING_TIME" "PAL_EGG_DEFAULT_HATCHING_TIME"
-replace_config_value "WorkSpeedRate" "$WORK_SPEED_RATE" "WORK_SPEED_RATE"
-replace_config_value "bIsMultiplay" "$IS_MULTIPLAY" "IS_MULTIPLAY"
-replace_config_value "bIsPvP" "$IS_PVP" "IS_PVP"
-replace_config_value "ServerName" "$SERVER_NAME" "SERVER_NAME"
-replace_config_value "AdminPassword" "$ADMIN_PASSWORD" "ADMIN_PASSWORD"
-replace_config_value "ServerPassword" "$SERVER_PASSWORD" "SERVER_PASSWORD"
-replace_config_value "PublicIP" "$PUBLIC_IP" "PUBLIC_IP"
-
-mkdir -p "$DEST_PATH"
-
-  cp "$LOCAL_CONFIG" "$DEST_CONFIG"
-
+  # Step 2: Start PalServer.sh script in the background
   bash "${STEAMAPPDIR}/PalServer.sh" \
+      -EpicApp="PalServer" \
+      -players=32 \
+      -useperfthreads \
+      -NoAsyncLoadingThread \
+      -UseMultithreadForDS &
+
+  # Step 3: Wait for PalServer.sh script to complete
+  wait "$(pgrep PalServer.sh)"
+
+  # Step 4: Stop the PalServer.sh script
+  pkill -f "PalServer.sh"
+
+  # Step 5: Copy contents of DefaultPalWorldSettings.ini to PalWorldSettings.ini
+  cp "$DEFAULT_CONFIG" "$DEST_CONFIG"
+
+sed -i "s|^Difficulty=.*$|Difficulty=${DIFFICULTY:-None}," "$DEST_CONFIG"
+sed -i "s|^DayTimeSpeedRate=.*$|DayTimeSpeedRate=${DAY_TIME_SPEED_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^NightTimeSpeedRate=.*$|NightTimeSpeedRate=${NIGHT_TIME_SPEED_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^ExpRate=.*$|ExpRate=${EXP_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PalCaptureRate=.*$|PalCaptureRate=${PAL_CAPTURE_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PalDamageRateAttack=.*$|PalDamageRateAttack=${PAL_DAMAGE_RATE_ATTACK:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PalDamageRateDefense=.*$|PalDamageRateDefense=${PAL_DAMAGE_RATE_DEFENSE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PlayerDamageRateAttack=.*$|PlayerDamageRateAttack=${PLAYER_DAMAGE_RATE_ATTACK:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PlayerDamageRateDefense=.*$|PlayerDamageRateDefense=${PLAYER_DAMAGE_RATE_DEFENSE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PlayerStomachDecreaseRate=.*$|PlayerStomachDecreaseRate=${PLAYER_STOMACH_DECREASE_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PlayerStaminaDecreaseRate=.*$|PlayerStaminaDecreaseRate=${PLAYER_STAMINA_DECREASE_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PalStomachDecreaseRate=.*$|PalStomachDecreaseRate=${PAL_STOMACH_DECREASE_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^PalStaminaDecreaseRate=.*$|PalStaminaDecreaseRate=${PAL_STAMINA_DECREASE_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^CollectionDropRate=.*$|CollectionDropRate=${COLLECTION_DROP_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^DeathPenalty=.*$|DeathPenalty=${DEATH_PENALTY:-All}," "$DEST_CONFIG"
+sed -i "s|^bEnableInvaderEnemy=.*$|bEnableInvaderEnemy=${ENABLE_INVADER_ENEMY:-True}," "$DEST_CONFIG"
+sed -i "s|^BaseCampWorkerMaxNum=.*$|BaseCampWorkerMaxNum=${BASE_CAMP_WORKER_MAX_NUM:-15}," "$DEST_CONFIG"
+sed -i "s|^PalEggDefaultHatchingTime=.*$|PalEggDefaultHatchingTime=${PAL_EGG_DEFAULT_HATCHING_TIME:-72.000000}," "$DEST_CONFIG"
+sed -i "s|^WorkSpeedRate=.*$|WorkSpeedRate=${WORK_SPEED_RATE:-1.000000}," "$DEST_CONFIG"
+sed -i "s|^bIsMultiplay=.*$|bIsMultiplay=${IS_MULTIPLAY:-False}," "$DEST_CONFIG"
+sed -i "s|^bIsPvP=.*$|bIsPvP=${IS_PVP:-False}," "$DEST_CONFIG"
+sed -i "s|^ServerName=.*$|ServerName=${SERVER_NAME:-Default Palworld Server}," "$DEST_CONFIG"
+sed -i "s|^AdminPassword=.*$|AdminPassword=${ADMIN_PASSWORD}," "$DEST_CONFIG"
+sed -i "s|^ServerPassword=.*$|ServerPassword=${SERVER_PASSWORD}," "$DEST_CONFIG"
+sed -i "s|^PublicIP=.*$|PublicIP=${PUBLIC_IP}," "$DEST_CONFIG"
+
+bash "${STEAMAPPDIR}/PalServer.sh" \
       -EpicApp="PalServer" \
       -players=32 \
       -useperfthreads \
       -NoAsyncLoadingThread \
       -UseMultithreadForDS
 
+  # Step 8: Keep the container running
   tail -f /dev/null
 else
   echo "Failed to install the PalWorld app. Exiting."
   exit 1
 fi
+
